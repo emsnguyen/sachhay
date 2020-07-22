2ca03512dd1cd4911363f7616a6ac77622ed752f
emsnguyen <hongmienft98@gmail.com>
2020-07-22 22:49:35 +0700

    Add homepage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "BookController@index");
+Route::view('/', "layouts.app");
 Route::group(['prefix'=>'dashboard'], function(){
     Route::view('/','dashboard/dashboard');
     // book routes
